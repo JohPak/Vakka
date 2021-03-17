bf597ff0dbff7a5cb65e6eac72cb2cb33b712ba7
JohannaP <55647359+JohPak@users.noreply.github.com>
2021-03-17 16:04:26 +0200

    hakumäärän korjauksia

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const fetch = require("node-fetch");
 const port = 8080
 const path = require('path');
 
+// MODUULIT
+const search = require('./modules/search.js');
+
 
 
 // for parsing application/xwww-form-urlencoded
@@ -15,8 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true })); // tarvitaan että lomakkeel
 // app.use(express.urlencoded()); // to support URL-encoded bodies
 
 
-// MODUULIT
-const search = require('./modules/search.js');
 
 // SERVE STATIC FILES Now, you can load the files that are in the public directory
 // app.use(express.static('public')) 
@@ -27,10 +28,11 @@ app.use(express.static(__dirname)); //NODEN käynnistyssijainti - saattaa muuttu
 
 // MUUT
 let word = "";
-let resultamount = 3; //haettavien hakutulosten määrä
+let resultamount = ""; 
 let filterresultsby = ""; // minkä perusteella tulokset järjestetään
 let html = fs.readFileSync("./views/main.html").toString("utf-8");
 let withoutimages = "";
+let productarray = [];
 let searchresult = {
     brandi: "",
     price: "",
@@ -44,9 +46,7 @@ let searchresult = {
     category: "",
     url: "",
     name: "",
-
 };
-let productarray = [];
 
 app.get('/', (req, res) => {
     console.log(req.body);
@@ -65,7 +65,7 @@ app.post('/', (req, res) => {
     console.log("---------------------------------------------------------------------------");
     console.log("POST KUTSU index.js, hakusana: " + req.body.hakusana);
     word = req.body.hakusana;
-    resultamount = req.body.hakumaara;
+    resultamount = (req.body.hakumaara < 10) ? 10 : req.body.hakumaara; //oletushakumäärä
     productarray.length = 0; //tyhjätään taulukko uutta hakua varten
     filterresultsby = req.body.sorttaus;
     withoutimages = req.body.kuvattomat;
@@ -74,38 +74,42 @@ app.post('/', (req, res) => {
     // huomaa hakusanan muutos enkoodatuksi! Muuten haku ei toimi ääkkösillä
     let url = `https://eucs13.ksearchnet.com/cloud-search/n-search/search?ticket=klevu-15596371644669941&term=${encodeURIComponent(word)}&paginationStartsFrom=0&sortPrice=false&ipAddress=undefined&analyticsApiKey=klevu-15596371644669941&showOutOfStockProducts=true&klevuFetchPopularTerms=false&klevu_priceInterval=500&fetchMinMaxPrice=true&klevu_multiSelectFilters=true&noOfResults=${resultamount}&klevuSort=${filterresultsby}&enableFilters=false&filterResults=&visibility=search&category=KLEVU_PRODUCT&sv=229&lsqt=&responseType=json`
     
+
         search.haku(url, {}) 
         .then(data => { 
         //   console.log(data); // JSON data parsed by `data.json()` call
-
-        // jos hakusana on <=1 merkkiä, pudotetaan haettavien tulosten määrä nollaksi
+        
+        // jos hakusana on <=1 merkkiä, herjataan
         if (word.length <=1) {
             console.log("hakusanan pituus liian lyhyt");
             sendThis(html.replace(/(?<=\<div class="main">)(.*?)(?=\<\/div>)/g, `<span class="virhe">Hakusana liian lyhyt/puuttuu</span>`));
         }
         let findingsamount = data.meta.totalResultsFound;
-
+        
         // TÄMÄ TÄRKEÄ. Mikäli for-lause yrittää luupata enemmän hakutuloksia kuin niitä on tarjolla, haku kaatuu.
         // jos hakumäärä > löydettyjen määrä, muuta hakumäärä löydettyjen määräksi
-        resultamount = (resultamount > findingsamount) ? findingsamount : resultamount;
+        resultamount = (resultamount > findingsamount) ? findingsamount : resultamount;    
 
-
-    try {
         for (let i = 0; i < resultamount; i++) {
-            
-            if (data.result[i].price != undefined && data.result[i].price != "") {
-                searchresult.price = `<span class="normihinta">${parseFloat(data.result[i].price).toFixed(2)} €</span>`;
-            }
-                searchresult.saleprice = `${parseFloat(data.result[i].salePrice).toFixed(2)} €`;
-                // poistetaan normihinta, mikäli se on sama kuin saleprice
-                if (parseFloat(data.result[i].salePrice).toFixed(2) == parseFloat(data.result[i].price).toFixed(2)) {
-                    searchresult.price = "";
+            try {
+                if (data.result[i].price != undefined && data.result[i].price != "") {
+                    searchresult.price = `<span class="normihinta">${parseFloat(data.result[i].price).toFixed(2)} €</span>`;
                 }
-                // jos tarjoushinta on voimassa, muutetaan tarjoushinta punaiseksi
-                else if (data.result[i].price) {
-                    searchresult.saleprice = `<span class="punaisella">ale ${parseFloat(data.result[i].salePrice).toFixed(2)} €</span>`;
-                }
-         
+                    searchresult.saleprice = `${parseFloat(data.result[i].salePrice).toFixed(2)} €`;
+                    // poistetaan normihinta, mikäli se on sama kuin saleprice
+                    if (parseFloat(data.result[i].salePrice).toFixed(2) == parseFloat(data.result[i].price).toFixed(2)) {
+                        searchresult.price = "";
+                    }
+                    // jos tarjoushinta on voimassa, muutetaan tarjoushinta punaiseksi
+                    else if (data.result[i].price) {
+                        searchresult.saleprice = `<span class="punaisella">ale ${parseFloat(data.result[i].salePrice).toFixed(2)} €</span>`;
+                    }
+            } catch (error) {
+                console.log("HINTAHAUSSA virhe " + error);
+                sendThis(html.replace(/(?<=\<div class="main">)(.*?)(?=\<\/div>)/g, `<span class="virhe"><h2>Noniin</h2>Nyt sä rikoit sen.<br><br>${error}<br><br>${JSON.stringify(data)}</span>`));
+               }
+
+            try {
                 // laitetaan oletuskuva, mikäli kuvaa ei ole
                 // var beverage = (age >= 21) ? "Beer" : "Juice";
                 searchresult.image = (data.result[i].image == "") ? "https://www.minimani.fi/media/catalog/product/placeholder/default/minimaniph.png" : data.result[i].image.replace("needtochange/","");
@@ -121,7 +125,12 @@ app.post('/', (req, res) => {
                 if (!data.result[i].weight) {
                     searchresult.weight = `<span class="puuttuu">ei painoa</span>`
                 }
+            } catch (error) {
+                console.log("KUVA, VARASTO, MAGENTOID, PAINO, EAN: virhe " + error);
+                sendThis(html.replace(/(?<=\<div class="main">)(.*?)(?=\<\/div>)/g, `<span class="virhe"><h2>Noniin</h2>Nyt sä rikoit sen.<br><br>${error}<br><br>${JSON.stringify(data)}</span>`));
+               }
 
+            try {
                 //siivotaan ryönää pois
                 searchresult.wholecategory = data.result[i].klevu_category.replace(`KLEVU_PRODUCT;;`, "").replace(`KLEVU_PRODUCT`, "").replace(`@ku@kuCategory@ku@`, "");
         
@@ -142,7 +151,13 @@ app.post('/', (req, res) => {
                             searchresult.category = searchresult.category.replace(`;`, ` > `);
                         }
                 }
-                
+                // liitetään kategoriaan linkki
+                searchresult.category = `<a href="/" onclick="clickCat(${searchresult.category})">${searchresult.category}</a>`;
+            } catch (error) {
+                console.log("KATEGORIAHAUSSA virhe " + error);
+                sendThis(html.replace(/(?<=\<div class="main">)(.*?)(?=\<\/div>)/g, `<span class="virhe"><h2>Noniin</h2>Nyt sä rikoit sen.<br><br>${error}<br><br>${JSON.stringify(data)}</span>`));
+               }
+
                 searchresult.url = data.result[i].url;
                 searchresult.name = data.result[i].name;
 
@@ -154,14 +169,9 @@ app.post('/', (req, res) => {
 
                 searchresult = {}; // luodaan uusi objekti seuraavaa tuotetta varten
                 
- 
-
         } //END FOR
 
-    } catch (error) {
-         console.log("VIRHETTÄ pukkaa, katopas: " + error);
-         sendThis(html.replace(/(?<=\<div class="main">)(.*?)(?=\<\/div>)/g, `<span class="virhe"><h2>Noniin</h2>Nyt sä rikoit sen.<br><br>${error}<br><br>${JSON.stringify(data)}</span>`));
-        }
+    
         // console.log(productarray);
             if (filterresultsby == "newfirst") {
                 // sortataan productarray magentoidn mukaan (UUSIMMAT ENSIN)
